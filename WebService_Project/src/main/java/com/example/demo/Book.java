e20809e722bfef1df70cf3e77bd659b45516ba6e
1NsomN1aTop <94468023+1NsomN1aTop@users.noreply.github.com>
2021-11-16 19:09:51 +0300

    Add files via upload

---
+++ WebService_Project/src/main/java/com/example/demo/Book.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.List;
+
+@Entity
+public class Book {
+    @javax.persistence.Id
+    @Column(name = "identifier", nullable = false)
+    private Long identifier;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    public Long getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(Long identifier) {
+        this.identifier = identifier;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long parseLong) {
+    }
+
+    public interface BookRepository extends CrudRepository<Book, Long> {
+        List<Book> findByTitle(String title);
+    }
+}
