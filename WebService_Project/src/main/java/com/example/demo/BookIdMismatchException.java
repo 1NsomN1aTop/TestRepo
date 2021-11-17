27440801f9b6596425289e89d95cfee9a5529153
1NsomN1aTop <94468023+1NsomN1aTop@users.noreply.github.com>
2021-11-17 10:27:06 +0300

    Add files via upload

---
+++ WebService_Project/src/main/java/com/example/demo/BookIdMismatchException.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class BookIdMismatchException extends Throwable {
+    public BookIdMismatchException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BookIdMismatchException() {
+
+    }
+}
---
+++ WebService_Project/src/main/java/com/example/demo/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class BookNotFoundException extends Throwable {
+
+    public BookNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    // ...
+}
