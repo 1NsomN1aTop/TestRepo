46398aa0db7d9395b611c55ddf0aa7c249654ec1
1NsomN1aTop <94468023+1NsomN1aTop@users.noreply.github.com>
2021-11-17 10:25:46 +0300

    Delete WebService_Project directory

--- WebService_Project/src/main/java/com/example/demo/BookNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-public class BookNotFoundException extends Throwable {
-
-    public BookNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-    // ...
-}
